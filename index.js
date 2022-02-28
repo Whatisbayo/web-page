ec78092b91381a05dfc00416ce9f4c85d03cd872
adebayo <awosunleadebayo@gmail.com>
2022-02-28 09:21:05 +0000

    BlackJack Game

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstCard = 6  
+
+let changeEl = document.getElementById("changee")
+
+function change(){
+    changeEl.innerText = Date() + ". " + "Please call me"
+}
+
+function hide(){
+    document.getElementById("changee").style.display = "none"
+
+}
